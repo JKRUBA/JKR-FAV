880dba2f672eeab2be4e2fb9ee31de6b9d7dbb5a
JEYA KRISHNAN M <mjkruba0608@gmail.com>
2024-10-08 00:39:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+var myImage=document.querySelector("#img1");
+
+function firstimg()
+{
+    myImage.src="GOOD DAY.jpg";
+}
+function secondimg()
+{
+    myImage.src="lemon-rice-stovetop-featured.jpg";
+}
+function thirdimg()
+{
+    myImage.src="semiya.webp";
+}
+function fourimg()
+{
+    myImage.src="sugarless-palkova-sweet.jpg";
+}
+function fiveimg()
+{
+    myImage.src="SLIPPEER.jpg";
+}
+function siximg()
+{
+    myImage.src="Seeded-Wedge-scaled.jpg";
+}
+function sevenimg()
+{
+    myImage.src="chapathi with chicken.jpg";
+}
+function eightimg()
+{
+    myImage.src="BAROTTA.jpg";
+}
+function nineimg()
+{
+    myImage.src="red-color-solid-background-1920x1080.png";
+}
+function tenimg()
+{
+    myImage.src="when-aishwarya-rai-campaigned-for-the-taj-mahal-to-be-voted-v0-qh49inat4lua1.webp";
+}
+function elevenimg()
+{
+    myImage.src="va.jpg";
+}
+function twelveimg()
+{
+    myImage.src="bike.webp";
+}
